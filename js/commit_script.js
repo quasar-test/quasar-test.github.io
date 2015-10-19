43c3793b37d7e85fb31f53f41c8804763ab5c12b
Joe Jean <joeclef@hotmail.com>
2015-10-19 11:31:38 +0400

    first commit

---
+++ js/commit_script.js
@@ -0,0 +1,30 @@
+//Class to represent a Commit Object
+function Commit(data) {
+
+    var self = this;
+    self.authorUrl = ko.observable(data.author.html_url);
+    self.authorAvatar = ko.observable(data.author.avatar_url);
+    self.message = ko.observable(data.commit.message);
+    self.date = ko.observable(moment(data.commit.committer.date).fromNow());
+    self.sha = ko.observable(data.commit.message);
+}
+
+//ViewModel for the commit
+function CommitViewModel(){
+    var self = this;
+    self.today = ko.observable(moment().format("dddd, MMMM Do YYYY"));
+    self.commitList = ko.observableArray();
+
+        $.getJSON("https://api.github.com/repos/slamdata/quasar/commits", function(commits){
+            for(var i=0; i < 2; i++){
+                com = new Commit(commits[i]);
+                self.commitList.push(com);
+            }
+    });
+}
+
+$(document).ready(function(){
+
+    ko.applyBindings(new CommitViewModel(), document.getElementById("commit"));
+
+});
